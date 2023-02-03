02f182d3a13f321cf51fb8de2aba0c4aabe99ea3
jimmyarq <120688833+jimmyarq@users.noreply.github.com>
2023-02-03 17:58:07 -0600

    Tarea Tan
    03 Feb 2023

--- web/codigo-otros-1-main/assets/js/app.js
+++ web/codigo-otros-1-main/assets/js/app.js
@@ -1,22 +1,26 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = await fetch ('https://api.github.com');
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $l = document.querySelector('#location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data = await response.json();
+    $n.textContent = data.name;
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
